8fc5eafe4a708ae5e33b3b3c7a50d1d35304ac9b
Simón Iribarren <tu-email@ejemplo.com>
2026-04-05 21:19:42 -0400

    Rebuild rollback site after mail form crash

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,7 @@
 import { defineConfig } from 'astro/config';
-import node from '@astrojs/node';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
-  output: 'server',
-  adapter: node({
-    mode: 'standalone',
-  }),
   site: 'https://phx.cl',
   integrations: [tailwind()],
 });
