f5907be2e051f94eb567946be6a092602d323751
Simón Iribarren <tu-email@ejemplo.com>
2026-04-05 21:08:36 -0400

    Add mail setup to contact form

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,15 @@
 /// <reference path="../.astro/types.d.ts" />
+
+interface ImportMetaEnv {
+  readonly SMTP_HOST: string;
+  readonly SMTP_PORT: string;
+  readonly SMTP_USER: string;
+  readonly SMTP_PASS: string;
+  readonly CONTACT_FROM_EMAIL: string;
+  readonly CONTACT_FROM_NAME: string;
+  readonly CONTACT_TO_EMAILS: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
