c82920b6e044a2f657e509f906c7007a4342ddc6
Simón Iribarren <tu-email@ejemplo.com>
2026-04-07 18:09:34 -0400

    Change form setup & msg

--- contact-endpoint-php/private/contact-config.php
+++ contact-endpoint-php/private/contact-config.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 return [
     'TURNSTILE_SECRET_KEY' => '0x4AAAAAAC1kSy-oGuJhYGC3C1JsEuT2u3E',
-    'SMTP_HOST' => 'mail.example.com',
+    'SMTP_HOST' => 'mail.phigital.cl',
     'SMTP_PORT' => '587',
     'SMTP_USERNAME' => 'contacto@phigital.cl',
     'SMTP_PASSWORD' => 'Secret0.!',
